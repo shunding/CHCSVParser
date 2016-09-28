6f877b79e722749eb75b8d402c015eebe6d0c7f9
Alex Drozhak <alex.drozhak@me.com>
2016-09-28 19:21:44 +0300

    Wrapped into iOS framework

---
+++ CHCSVParser-iOS/CHCSVParser-iOS.h
@@ -0,0 +1,21 @@
+//
+//  CHCSVParser-iOS.h
+//  CHCSVParser-iOS
+//
+//  Created by Alex Drozhak on 9/28/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CHCSVParser-iOS.
+FOUNDATION_EXPORT double CHCSVParser_iOSVersionNumber;
+
+//! Project version string for CHCSVParser-iOS.
+FOUNDATION_EXPORT const unsigned char CHCSVParser_iOSVersionString[];
+
+#import <CHCSVParser_iOS/CHCSVParser.h>
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHCSVParser_iOS/PublicHeader.h>
+
+
